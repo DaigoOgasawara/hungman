da31536d6f79a6f751c19edb1c63384f4c8cde2c
Daigo Ogasawara <daigoogasawara@DiagoMBP.local>
2021-01-06 22:28:17 +0900

    this is my first commit

---
+++ skmdk.py
@@ -0,0 +1,15 @@
+
+songs = {"1":"fun",
+	 "2":"blue",
+	 "3":"me",
+	 "4":"floor",
+	 "5":"live"
+         }
+
+n = input("文字列を入力して下さい。")
+
+if n in songs:
+	song = songs[n]
+	print(song)
+else:
+	print("見つかりません。")
